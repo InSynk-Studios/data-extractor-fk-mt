cc72b9e2cde3aa00bf8ebd78cab24490878561e4
rupayan <rupayan@insynkstudios.com>
2024-02-15 18:29:25 +0530

    Add headless option and set user agent and extra headers

--- index.js
+++ index.js
@@ -23,8 +23,14 @@ export async function getProductDetails(url) {
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
+    headless: true,
   });
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36');
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'en-US,en;q=0.9'
+  });
+
   await page.goto(url, { waitUntil: 'networkidle2' });
   const html = await page.content();
   console.log(html);
